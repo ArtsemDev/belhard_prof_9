e2cad79cd0f05701cc3ffa0af23f6439bf4aa4d0
milvus <thommy.python.dev@gmail.com>
2022-12-08 19:24:24 +0300

    urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+from .models import Post
+
+
+def blog_list(request: HttpRequest):
+    return HttpResponse('<br><br><b>Hello</b>')
+
+
+def post_detail(request: HttpRequest, post_slug: str):
+    post = Post.objects.get(slug=post_slug)
+    return HttpResponse(f'<br><br><b>{post.title}</b>')
